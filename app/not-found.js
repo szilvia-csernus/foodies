f31c216ead6e466dc4d1715ea15429189ecf4723
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-07-28 17:23:31 +0100

    add error pages

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not Found</h1>
+      <p>Sorry, the page you are looking for is not found.</p>
+    </main>
+  );
+}
