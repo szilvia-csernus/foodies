439cadee0fc088709a4cbd8d14f491ea17d83f5f
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-07-28 17:29:05 +0100

    add mealSlug page and styles

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,35 @@
-export default function MealDetails({ params }) {
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  const instructions = meal.instructions.replace(/\n/g, "<br>",);
+
   return (
-    <main>
-      <h1>Meal Details</h1>
-      <p>{params.mealSlug}</p>
-    </main>
+    <>
+    <header className={classes.header}>
+      <div className={classes.image}>
+        <Image src={meal.image} alt={meal.title} fill />
+      </div>
+      <div className={classes.headerText}>
+        <h1>{meal.title}</h1>
+        <p>by <a className={classes.creator} href={`mailto:${meal.creator_email}`}>{meal.creator}</a></p>
+        <p className={classes.summary}>{meal.summary}</p>
+      </div>
+
+    </header>
+      <main>
+        <p className={classes.instructions} dangerouslySetInnerHTML={{
+          __html: instructions,
+        }}></p>
+      </main>
+    </>
   );
 }
